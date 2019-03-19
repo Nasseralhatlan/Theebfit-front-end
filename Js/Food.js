20feadf81d2d42ac2441b5c5951df58e6ba9fd59
Nasser on mac <nasser@Nassers-MacBook-Pro.local>
2019-03-20 02:03:08 +0300

    Working on Search food

--- Js/Food.js
+++ Js/Food.js
@@ -1,13 +1,30 @@
 $(document).ready(function() {
-  function HideAllFood() {
-    $("#FOOD-MACROS-DIV").hide();
-    $("#FOOD-LIST-DIV").hide();
-    $("#ADD-FOOD-BTN").hide();
-  }
 
-  function ShowAll() {
+  
+    function HideAllFood() {
+     $("#FOOD-MACROS-DIV").hide();
+     $("#FOOD-LIST-DIV").hide();
+     $("#SEARCH-FOOD").hide();
+    }
+
+
+    HideAllFood();
     $("#FOOD-MACROS-DIV").show();
     $("#FOOD-LIST-DIV").show();
-    $("#ADD-FOOD-BTN").show();
-  }
+  
+
+
+
+
+$("#ADD-FOOD-BTN").click(function (){ 
+    HideAllFood();
+    $("#SEARCH-FOOD").show();
+    $("body,html").animate({scrollTop:0},500);
+});
+
+ 
+
+
+
+
 });
