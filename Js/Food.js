1b16e5bbd14ba4311b5d37d8de7e8c22f609d52d
Nasser on mac <nasser@Nassers-MacBook-Pro.local>
2019-03-26 02:44:15 +0300

    Cam css edit

--- Js/Food.js
+++ Js/Food.js
@@ -32,6 +32,77 @@ $("#SEARCH-BACBTN").click(function () {
 
 
 
+ 
+$("#CAM-CAN-BTN").click(function () { 
+    Quagga.stop();
+    $("#cam").hide();
+    $("#SEARCH-FOOD").removeClass("HIDE");    
+});
+
+$("#SCAN-BARCODE-BTN").click(function(){ 
+
+$("#SEARCH-FOOD").addClass("HIDE");
+
+Quagga.init({
+inputStream : {
+  name : "Live",
+  type : "LiveStream",
+  target: document.querySelector('#cam')    // Or '#yourElement' (optional)
+},
+
+decoder : {
+  readers : ["ean_reader"]
+//   [
+//   "code_128_reader",
+//   "ean_reader",
+//   "ean_8_reader",
+//   "upc_reader",
+//   "upc_e_reader",
+// ]
+}
+
+}, function(err){
+
+
+
+if (err){
+    console.log(err);
+    Quagga.stop();
+    $("#cam").hide();
+    $("#SEARCH-FOOD").removeClass("HIDE");
+    return
+}
+
+  console.log("Initialization finished. Ready to start");
+    $("#cam").show();
+    $("video").attr("width","");
+    $("video").attr("height","");
+    $("canvas").hide();
+    $("br").hide();
+    $("video").addClass("CAM");
+    Quagga.start();
+    console.log("started");
+
+
+    Quagga.onDetected(function(data){
+    console.log(data.codeResult.code);
+  
+     
+    var text = data.codeResult.code ; 
+    
+
+    $("#SEARCH-INPUT").val(text);
+    $("#cam").hide();
+    $("#SEARCH-FOOD").removeClass("HIDE");
+
+    Quagga.stop();
+
+     
+   });
+
+  });
+});
+
 
 
 });
