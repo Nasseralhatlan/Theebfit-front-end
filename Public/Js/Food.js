55c73d271cd365e6f5288647fa60a205e89f94b8
Nasser on Pc <you@example.com>
2019-04-28 22:06:58 +0300

    Finish refactoring the food section

--- Public/Js/Food.js
+++ Public/Js/Food.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
 
     startFoodPoint();
@@ -6,23 +6,22 @@ $(document).ready(function() {
   
     function HideAllFood(){
      $("#FOOD-MACROS-DIV").hide();
-     $("#FOOD-LIST-DIV").hide();
+     $("#FOOD-LIST-MAIN-DIV").hide();
      $("#SEARCH-FOOD").hide();
      $("#EDIT-MACROS-DIV").hide();
-     $("#EDIT-MACROS-DIV").hide();
-     $("#FOOD-INFO").hide();
-     $("#cam").hide();
-    } 
+     $("#FOOD-INFO-ADDED").hide();
+     $("#FOOD-INFO-SEARCH").hide();
 
+    } 
 
     function addClassHideAll(){
       $("#FOOD-MACROS-DIV").addClass("HIDE");
-      $("#FOOD-LIST-DIV").addClass("HIDE");
+      $("#FOOD-LIST-MAIN-DIV").addClass("HIDE");
       $("#SEARCH-FOOD").addClass("HIDE");
       $("#EDIT-MACROS-DIV").addClass("HIDE");
-      $("#FOOD-INFO").addClass("HIDE");
+      $("#FOOD-INFO-ADDED").addClass("HIDE");
+      $("#FOOD-INFO-SEARCH").addClass("HIDE");
     }
-
 
     function ShowOne(id){
       $("body,html").animate({scrollTop:0},0);
@@ -38,10 +37,10 @@ $(document).ready(function() {
       addClassHideAll();
       $("body,html").animate({scrollTop:0},0);
       $("#FOOD-MACROS-DIV").show();
-      $("#FOOD-LIST-DIV").show();
+      $("#FOOD-LIST-MAIN-DIV").show();
       setTimeout(function(){  
         $("#FOOD-MACROS-DIV").removeClass("HIDE");
-        $("#FOOD-LIST-DIV").removeClass("HIDE");
+        $("#FOOD-LIST-MAIN-DIV").removeClass("HIDE");
       },50);
     }
 
@@ -87,99 +86,36 @@ $("#SAVE-EDIT-MACROS-BTN").click(function () {
 });
 
 
-$("#BACK-FOOD-INFO-BTN").click(function () { 
+$("#BACK-ADDED-FOOD-INFO-BTN").click(function () { 
   startFoodPoint();
 });
 
-$("#SAVE-FOOD-INFO-BTN").click(function () { 
+$("#SAVE-ADDED-FOOD-INFO-BTN").click(function () { 
   startFoodPoint();
 });
 
-
-$("#CAM-CAN-BTN").click(function () { 
-    Quagga.stop();
-    $("#cam").hide();
-    ShowOne("#SEARCH-FOOD");    
-});
-
-
-$(".FOOD").click(function (){  
-  ShowOne("#FOOD-INFO");
+$(".ADDED-FOOD").click(function (){  
+  ShowOne("#FOOD-INFO-ADDED");
 });
 
 
 
 
-
-$("#SCAN-BARCODE-BTN").click(function(){ 
-
-  HideAllFood();
-  addClassHideAll();
-
-Quagga.init({
-inputStream : {
-  numOfWorkers: navigator.hardwareConcurrency,
-  name : "Live",
-  type : "LiveStream",
-  target: document.querySelector('#cam')    // Or '#yourElement' (optional)
-},
-
-decoder : {
-  readers : ["ean_reader"]
-//   [
-//   "code_128_reader",
-//   "ean_reader",
-//   "ean_8_reader",
-//   "upc_reader",
-//   "upc_e_reader",
-// ]
-}
-
-}, function(err){
-
-
-
-if (err){
-    console.log(err);
-    alert("Cam is not found or Cam Accsess is rejected if you want to reuse the scanner you have to log out and log in agian and allow Accsess");
-    Quagga.stop();
-    HideAllFood();
-    addClassHideAll();
-    ShowOne("#SEARCH-FOOD");
-    return
-}
-
-  console.log("Initialization finished. Ready to start");
-    $("#cam").show();
-    $("video").attr("width","");
-    $("video").attr("height","");
-    $("canvas").hide();
-    $("br").hide();
-    $("video").addClass("CAM");
-
-
-
-    Quagga.start();
-    console.log("started");
-
-
-    Quagga.onDetected(function(data){
-    console.log(data.codeResult.code);
-  
-     
-    var text = data.codeResult.code ; 
-    
-
-    $("#SEARCH-INPUT").val(text);
-    $("#cam").hide();
-    ShowOne("#SEARCH-FOOD");
-
-    Quagga.stop();
-     
-   });
-
-  });
+$("#BACK-SEARCH-FOOD-INFO-BTN").click(function () { 
+  ShowOne("#SEARCH-FOOD");
 });
+
+$("#ADD-SEARCH-FOOD-INFO-BTN").click(function () { 
+  startFoodPoint();
+});
+
+$(".SEARCH-RESULT-FOOD").click(function (){  
+  ShowOne("#FOOD-INFO-SEARCH");
+});
+
+
+
+
 
 
 
